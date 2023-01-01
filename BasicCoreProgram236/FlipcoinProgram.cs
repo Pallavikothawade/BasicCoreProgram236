b09eceb2b40a1f7de3d5ff55b7a386abb6873a0d
Pallavikothawade <Pallavijkothawade@gmail.com>
2023-01-01 22:02:38 +0530

    Adding The Flipcoin

---
+++ BasicCoreProgram236/FlipcoinProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram236
+{
+    class FlipcoinProgram
+    {
+        public void HeadTail()
+        {
+            int tailcount = 0, headcount = 0;
+            Console.WriteLine("Number of Times to the flipcoin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+
+            for (int i = 1; i <= n; i++)
+            {
+                double input = random.NextDouble();
+                if (input < 0.5)
+                    tailcount++;
+                else
+                    headcount++;
+            }
+            Console.WriteLine("Tailcount:{0} Headcount:{1}", tailcount, headcount);
+            double tailpercentage = (tailcount * 100) / 10;
+            double headpercentage = (headcount * 100) / 10;
+            Console.WriteLine("Tail={0}% headpercentage={1}%", tailpercentage, headpercentage);
+            Console.ReadLine();
+        }
+    }
+}
--- BasicCoreProgram236/Program.cs
+++ BasicCoreProgram236/Program.cs
@@ -12,7 +12,19 @@ namespace BasicCoreProgram236
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome The Basic Program");
-            Console.ReadLine();
+            Console.WriteLine("Enter The Choose One Option");
+            Console.WriteLine("1:FlipCoin\n2:Checked Leap Year\n3:Factors\n4:Checked Remainder\n5:Swap Two Numbers\n6:Vowels and Consonant\n7:Even Or Odd\n8:Large Number\n9:Harmonic Number\n10:Power Of Two\n");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    FlipcoinProgram fp = new FlipcoinProgram();
+                    fp.HeadTail();
+                    break;
+                //default:
+                //    break;
+            }
+                    Console.ReadLine();
         }
     }
 }
