5ea4415f06ca2029dff697b98ead983e72977c65
Pallavikothawade <Pallavijkothawade@gmail.com>
2023-01-01 21:46:32 +0530

    [Pallavikothawade]BasicCoreProgram

---
+++ BasicCoreProgram236/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram236
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome The Basic Program");
+            Console.ReadLine();
+        }
+    }
+}
